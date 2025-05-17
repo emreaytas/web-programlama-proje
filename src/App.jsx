57d886688c32414345c854a6da725df54989837f
Ahmet Yasin YILMAZ <ylmzayasin@gmail.com>
2025-05-17 21:34:32 +0300

    Added admin who can see, add and delete products

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import AdminPage from './pages/AdminPage';
+
+function AppContent() {
+  const location = useLocation();
+  const hideNavbar = location.pathname.startsWith("/admin");
+
+  return (
+    <>
+      {!hideNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <AppContent />
     </Router>
   );
 }
