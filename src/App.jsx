4cc89494f8c5ea098e739f6aebf68650d2ee3912
Ahmet Yasin YILMAZ <ylmzayasin@gmail.com>
2025-05-18 12:52:40 +0300

    Admin sayfası eklendi: ürün görüntüleme, ekleme ve silme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import AdminPage from "./pages/AdminPage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </Router>
   );
