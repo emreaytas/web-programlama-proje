7ddaf34b503cc050e49b4f8c7285cf2e1d64f73e
İsmail Özer <114927605+ismailozerrr@users.noreply.github.com>
2025-05-23 15:18:18 +0300

    CORS ve güvenlik ayarları eklendi.

---
+++ CORS.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy => policy.WithOrigins("http://localhost:3000")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseCors("AllowFrontend");
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
