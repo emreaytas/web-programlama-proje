1e8f9035bafe2b353e7c61eb8e9a9ec0a6afb6cd
İsmail Özer <114927605+ismailozerrr@users.noreply.github.com>
2025-05-23 15:09:41 +0300

    User modeli eklendi.

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+namespace webprogbackend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        public Cart? Cart { get; set; }
+    }
+}
